1a1a42a3da36e0e6c3821e850bf6c251f5ce5778
Alexey Agapitov <marwinxxii@gmail.com>
2012-02-18 15:07:53 +0400

    instantiating provider

--- src/com/icookbook/RecipeProviderFactory.java
+++ src/com/icookbook/RecipeProviderFactory.java
@@ -1,8 +1,10 @@
 package com.icookbook;
 
+import android.content.Context;
+
 public class RecipeProviderFactory {
 	
-	public RecipeProvider getProvider(String name) {
-		return null;
+	public RecipeProvider getProvider(String name, Context context) {
+		return new ResourceRecipeProvider(context);
 	}
 }
